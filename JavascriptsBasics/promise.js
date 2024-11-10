5d7361e8feb708c9142ce8ab88f7199a649c2d2d
Sayantansen123 <riosayan123@gmail.com>
2024-11-11 01:18:19 +0530

    promise

---
+++ JavascriptsBasics/promise.js
@@ -0,0 +1,35 @@
+const PromiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("async task is completed");
+        resolve()
+    }, 1000)
+})
+
+
+//do the wprk after promise is resolved 
+//if we dont use resolve the code inside PromiseOne.then() will not
+PromiseOne.then(() => {
+    console.log("hello your promise is resolved")
+})
+
+
+const PromiseTwo = new Promise((resolve, reject) => {
+    let error = false;
+    if (!error) {
+        console.log("async task is completed");
+        resolve({ name: "sayantan", age: 34 })
+    } else {
+        reject("something went wrong")
+    }
+})
+
+PromiseTwo
+    .then((e) => {
+        console.log(e)
+        return e.name
+    })
+    .then((e) => (console.log(e)))
+    .catch((e) => {
+        console.log(e)
+    })
+
