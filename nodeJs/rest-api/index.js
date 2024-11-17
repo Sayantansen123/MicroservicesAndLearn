efffd01a796655fd5d6625d1673617db6afc706e
Sayantansen123 <riosayan123@gmail.com>
2024-11-17 14:27:16 +0530

    dynamic url

--- nodeJs/rest-api/index.js
+++ nodeJs/rest-api/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 
+//REST ==> Represntational State Transfer
 
 const app = express()
 
@@ -24,8 +25,20 @@ app.get("/", (req, res) => {
   res.send("welcome to our book store")
 })
 
+//get all books
 app.get("/get", (req, res) => {
    res.send(books)
+})
+
+//get a single book
+app.get("/get/:id",(req,res)=>{
+    const bookId = req.params.id;
+    const singleBook = books.find(product => product.id.toString() === bookId.toString())
+    if(singleBook){
+        res.status(200).send(singleBook)
+    }else{
+        res.status(404).send("cant find the book")
+    }
 })
 
 app.listen(3000, () => {
