3b4e60aee852a3a4449d532deb684dd31cf01cf7
Sayantansen123 <riosayan123@gmail.com>
2024-11-18 10:52:21 +0530

    rest api done

--- nodeJs/rest-api/index.js
+++ nodeJs/rest-api/index.js
@@ -41,6 +41,49 @@ app.get("/get/:id",(req,res)=>{
     }
 })
 
+//post a new book
+app.post("/add",(req,res)=>{
+    const newBook = {
+        id: books.length + 1,
+        title : `books ${books.length + 1}`
+    }
+    books.push(newBook)
+    res.status(200).json({
+        data : newBook,
+        message : "Book is added"
+    })
+})
+
+//update a new book
+app.put("/update/:id",(req,res)=>{
+    const findCurrentBook = books.find((bookItem) => bookItem.id.toString() === req.params.id.toString())
+    if(findCurrentBook){
+        findCurrentBook.title = req.body.title || findCurrentBook.title;
+        res.status(200).send({
+            message : `book with id ${req.params.id} updated`,
+            book : books
+        })
+    }else{
+        res.status(404).send({
+            message : "book not found"
+        })
+    }
+})
+
+//delete a book
+app.delete("/delete/:id",(req,res)=>{
+    const findCurrentBook = books.findIndex((bookItem) => bookItem.id.toString() === req.params.id.toString())
+    if(findCurrentBook !== -1){
+        const deletedBook = books.splice(findCurrentBook,1)
+        res.status(200).json({
+            data : "Book deleted succesfully",
+            book: books
+        })
+    }else{
+        res.status(404).send("some error occured")
+    }
+})
+
 app.listen(3000, () => {
     console.log("server started on port http://localhost:3000")
 })
