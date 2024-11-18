a0705c3e642847dd27918eca771918ebf95bf6c5
Sayantansen123 <riosayan123@gmail.com>
2024-11-18 22:12:14 +0530

    auth-middleware created

--- nodeJs/authentication/routes/home-routes.js
+++ nodeJs/authentication/routes/home-routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/auth-middleware")
 
-router.get("/home", (req, res) => {
-    res.json({ message: "welcome to homepage" })
+router.get("/welcome",authMiddleware, (req, res) => {
+    const { username, userId, role } = req.userInfo;
+
+    res.json({
+      message: "Welcome to the home page",
+      user: {
+        _id: userId,
+        username,
+        role,
+      },
+    });
 })
 
 module.exports = router
