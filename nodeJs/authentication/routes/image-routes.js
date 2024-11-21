74fb73a761679e236033841d0a987332feec0565
Sayantansen123 <riosayan123@gmail.com>
2024-11-21 22:50:13 +0530

    adding middlewrae

--- nodeJs/authentication/routes/image-routes.js
+++ nodeJs/authentication/routes/image-routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const adminMiddleware = require("../middleware/admin-middleware")
+const authMiddleware = require("../middleware/auth-middleware")
 
 const router = express.Router();
 
 
-
+router.post("/upload",)
 
 module.exports = router;
