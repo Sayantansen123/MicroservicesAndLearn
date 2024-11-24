439dcc6ecd191f3c76ff65d4f7fa375f9304153d
Sayantansen123 <riosayan123@gmail.com>
2024-11-24 19:10:16 +0530

    mongodb connected succesfully

--- nodeJs/intermediateMongoDb/server.js
+++ nodeJs/intermediateMongoDb/server.js
@@ -1,3 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose")
+
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("MongoDb connected")
+}).catch((e) => console.log(e))
+
+//middleware 
+
+app.use(express.json())
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
