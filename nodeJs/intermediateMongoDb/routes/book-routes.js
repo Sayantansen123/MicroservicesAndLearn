db6020e449b39c5efc3866dc7bdfd5b8aa9f7909
Sayantansen123 <riosayan123@gmail.com>
2024-12-06 22:15:35 +0530

    minor changes

--- nodeJs/intermediateMongoDb/routes/book-routes.js
+++ nodeJs/intermediateMongoDb/routes/book-routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const {  insertSampleProducts, getProductsStats, getProductAnalytics } = require("../controllers/product-controller")
 
 const router = express.Router()
-
+//all the routes created
 router.post("/add", insertSampleProducts)
 router.get("/allproducts",getProductsStats)
 router.get("/analytics",getProductAnalytics)
